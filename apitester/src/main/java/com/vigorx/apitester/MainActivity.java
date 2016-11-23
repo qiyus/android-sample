891309eb6483930bb2c47f182ae86f0dcf23f63c
qiyus <hsly_song@sina.com>
2016-11-23 10:15:22 +0800

    review

---
+++ apitester/src/main/java/com/vigorx/apitester/MainActivity.java
@@ -0,0 +1,37 @@
+package com.vigorx.apitester;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.PopupMenu;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button popupbtn = (Button) findViewById(R.id.button);
+        popupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu menu = new PopupMenu(MainActivity.this, popupbtn);
+                menu.getMenuInflater().inflate(R.menu.menu_popupmenu, menu.getMenu());
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, R.string.popupmenu_message ,Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                });
+                menu.getMenu().add(Menu.NONE, Menu.FIRST + 6, 6, R.string.popupmenu_message);
+                menu.show();
+            }
+        });
+    }
+}
